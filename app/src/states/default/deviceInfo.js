7632e9cb5970c2893dfe75543261570d45ab639b
Rokas Vaitkevičius <vaitkevicius.rokas@gmail.com>
2018-12-09 16:51:36 +0200

    Added device info intent

---
+++ app/src/states/default/deviceInfo.js
@@ -0,0 +1,38 @@
+const DeviceInfoIntent = {
+  canHandle(handlerInput) {
+    const { request } = handlerInput.requestEnvelope
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'DeviceInfoIntent'
+  },
+  async handle(handlerInput) {
+    const {
+      responseBuilder,
+      attributesManager,
+      requestEnvelope,
+      serviceClientFactory,
+    } = handlerInput
+    const { speech, reprompt } = attributesManager.getRequestAttributes()
+    const { deviceId } = requestEnvelope.context.System.device
+
+    const upsServiceClient = serviceClientFactory.getUpsServiceClient()
+
+    const servicesPromiseArray = [
+      upsServiceClient.getSystemDistanceUnits(deviceId),
+      upsServiceClient.getSystemTemperatureUnit(deviceId),
+      upsServiceClient.getSystemTimeZone(deviceId),
+    ]
+
+    const [distanceUnits, temperatureUnits, timeZone] = await Promise.all(servicesPromiseArray.map(p => p.catch(e => e)))
+
+    speech.say('This is device info intent. Device info is displayed in the card.')
+    reprompt.say('Device info intent reprompt')
+
+    return responseBuilder
+      .speak(speech.ssml(true))
+      .reprompt(reprompt.ssml(true))
+      .withSimpleCard('Device Info', JSON.stringify({ distanceUnits, temperatureUnits, timeZone }, null, 2))
+      .getResponse()
+  },
+}
+
+export default DeviceInfoIntent
--- app/src/states/default/index.js
+++ app/src/states/default/index.js
@@ -3,11 +3,13 @@ import stopIntent from './stop'
 import unhandledIntent from './unhandled'
 import helpIntent from './help'
 import permissionsIntent from './permissions'
+import deviceInfoIntent from './deviceInfo'
 
 export default [
   launchRequest,
   stopIntent,
   helpIntent,
   permissionsIntent,
+  deviceInfoIntent,
   unhandledIntent,
 ]
